dd2e205a5182f0a3b59233eea33f18e1c9b10c48
Weston McNamee <ghost.squadron@gmail.com>
2014-07-17 20:41:49 -0700

    updating fileappender

--- Logging/Add-FileLogAppender.ps1
+++ Logging/Add-FileLogAppender.ps1
@@ -1,31 +1,36 @@
 function Add-FileLogAppender{
     [CmdletBinding()]
-    param
-    (
-        [parameter(ValueFromPipeline=$true)]
-        [log4net.ILog]$log,
+    param (
         [log4net.Core.Level]$logLevelThreshold = [log4net.Core.Level]::Info,
         [string]$logPattern = "%date{ISO8601} [%thread] %-5level [%ndc] - %message%newline",
         [string]$logPath = (Join-Path $PSScriptRoot "logs"),
         [string]$logFile = ((Split-Path $MyInvocation.PSCommandPath -Leaf) `
-                + ([DateTime]::UtcNow.ToString("s") -replace ":","-") + "Z.log")
+                + ([DateTime]::UtcNow.ToString("s") -replace ":","-") + "Z.log"),
+        [parameter(ValueFromPipeline=$true)]
+        [log4net.ILog]$log
     )
 
-    if($log -eq $null){
-        $logger = ([log4net.LogManager]::GetRepository() -as [log4net.Repository.Hierarchy.Hierarchy]).Root
-    }
-    else {
+    process {
+        Write-Debug "logLevelThreshold: $($logLevelTheshold.ToString())"
+        Write-Debug "logPattern: $logPattern"
+        Write-Debug "logPath: $logPath"
+        Write-Debug "logFile: $logFile"
+
+        if($log -eq $null){
+            $log = Get-Logger -callStackStart 2
+        }
+
         $logger = $log.Logger
+
+        $patternLayout = new-object log4net.Layout.PatternLayout($logPattern)
+        $fullLogFile = (Join-Path $logPath $logFile)
+        $fileAppender = New-Object log4net.Appender.FileAppender($patternLayout, $fullLogFile, $true)
+        $fileAppender.Threshold = $logLevelThreshold
+        $fileAppender.LockingModel = new-object log4net.Appender.FileAppender+MinimalLock
+        $fileAppender.ActivateOptions()
+
+        $logger.AddAppender($fileAppender)
     }
-
-    $patternLayout = new-object log4net.Layout.PatternLayout($logPattern)
-    $fullLogFile = (Join-Path $logPath $logFile)
-    $fileAppender = New-Object log4net.Appender.FileAppender($patternLayout, $fullLogFile, $true)
-    $fileAppender.Threshold = $logLevelThreshold
-    $fileAppender.LockingModel = new-object log4net.Appender.FileAppender+MinimalLock
-    $fileAppender.ActivateOptions()
-
-    $logger.AddAppender($fileAppender)
 }
 
 function AddConsoleLogAppender {
