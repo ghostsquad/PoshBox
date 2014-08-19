40f0382a29f91a4c2e6617b7227ba50b1013f151
Weston McNamee <ghost.squadron@gmail.com>
2014-08-19 15:38:09 -0700

    New-Interface allows psuedo-polymorphism

---
+++ New-Interface.ps1
@@ -0,0 +1,48 @@
+#http://csharpening.net/?p=1462
+function New-Interface
+{
+    param(
+    [String]$Name,
+    [PSCustomObject]$Definition
+    )
+
+    $InterfaceType = Add-Type -TypeDefinition "
+        public class $Name : System.Management.Automation.PSObject
+        {
+            public $Name(System.Management.Automation.PSObject obj) : base(obj)
+            {
+                if (Definition == null) throw new System.NullReferenceException(`"Defintion is null!`");
+                if (obj == null) throw new System.ArgumentNullException(`"Obj was null!`");
+
+                foreach(var interfaceMember in Definition.Members)
+                {
+                    if (interfaceMember == null) continue;
+
+                    bool found = false;
+                    foreach(var inheritorMember in obj.Members)
+                    {
+                        if (inheritorMember == null) continue;
+
+                        if(inheritorMember.IsInstance &amp;&amp;
+                           inheritorMember.MemberType == interfaceMember.MemberType &amp;&amp;
+                           inheritorMember.Name == interfaceMember.Name)
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        throw new System.InvalidCastException(`"The object is not of type $Name. Missing member `" + interfaceMember.Name);
+                    }
+                }
+            }
+
+            public static System.Management.Automation.PSObject Definition {get;set;}
+        }
+    " -PassThru
+    $acceleratorsType = [PSCustomObject].Assembly.gettype("System.Management.Automation.TypeAccelerators")
+    $acceleratorsType::Add($Name, $InterfaceType);
+    $InterfaceType::Definition = $Definition
+}
